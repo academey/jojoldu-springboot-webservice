5d810f82db2e18e59d69d657048c03bb08f497c2
academey <academey@gmail.com>
2020-07-04 00:11:06 +0900

    뷰단 처리 및 BaseTimeEntity, ResponseDto 등 생성

--- src/main/java/com/academey/book/springboot/web/domain/posts/PostsRepository.java
+++ src/main/java/com/academey/book/springboot/web/domain/posts/PostsRepository.java
@@ -1,6 +1,11 @@
 package com.academey.book.springboot.web.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+    @Query("SElECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
